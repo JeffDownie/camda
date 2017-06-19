008216b093d71ec27738289d55a7352f3337f701
Jeff Downie <jeff.downie@trinitymirror.com>
2017-06-19 11:46:19 +0100

    Moving all files that can cause case sensitivity problems
    See https://github.com/JeffDownie/camda/issues/4

--- test/index.js
+++ test/index.js
@@ -1,10 +1,23 @@
 'use strict';
-describe('CB', () => {
-    require('./CB');
+const assert = require('assert');
+
+describe('caller', () => {
+    require('./caller');
 });
-describe('cb', () => {
-    require('./cb');
+describe('callee', () => {
+    require('./callee');
 });
 describe('ramda integration', () => {
     require('./ramdaIntegration');
+});
+describe('module structure', () => {
+    const mod = require('../');
+    it('should have multiple names for the caller module', done => {
+        assert.ok(mod.CB == mod.caller);
+        done();
+    });
+    it('should have multiple names for the callee module', done => {
+        assert.ok(mod.cb == mod.callee);
+        done();
+    });
 });
